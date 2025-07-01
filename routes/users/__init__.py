8d5189a470aa6be1f9e2217f38119805856be604
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-01 22:03:03 +0100

    Add static folder to users blueprint
    Configured the users_blp Blueprint to include a static_folder argument, enabling static file serving for user-related routes.

--- routes/users/__init__.py
+++ routes/users/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-users_blp = Blueprint("users_blp", __name__, template_folder="../../templates")
+users_blp = Blueprint("users_blp", __name__, template_folder="../../templates", static_folder="../../static")
 
 from .authorized_route import *
 from .non_authorized_route import *
