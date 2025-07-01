9f42f0530aede941bbbdf34b2276c75841ff6f2d
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-01 22:02:37 +0100

    Add admin blueprint initialization
    Introduces the admin blueprint in the routes/admin package, setting up template and static folders, and imports users and properties modules.

---
+++ routes/admin/__init__.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+admin_blp = Blueprint("admin_blp", __name__, template_folder="../../templates", static_folder="../../static")
+
+from .users import *
+from .properties import *
