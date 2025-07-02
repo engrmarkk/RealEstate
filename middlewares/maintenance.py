dd8a1d8fe899b45949fb51d14568561626f40eb4
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-02 15:04:55 +0100

    Update maintenance.py

--- middlewares/maintenance.py
+++ middlewares/maintenance.py
@@ -4,11 +4,15 @@ from flask import redirect, request, url_for
 EXCLUDED_ENDPOINTS = ["error_blp.maintenance"]
 EXCLUDED_BLUEPRINTS = ["admin_blp", "error_blp"]
 
+
 def maintenance_check():
     if not MAINTENANCE_MODE:
         return
 
-    if request.blueprint in EXCLUDED_BLUEPRINTS or request.endpoint in EXCLUDED_ENDPOINTS:
+    if (
+        request.blueprint in EXCLUDED_BLUEPRINTS
+        or request.endpoint in EXCLUDED_ENDPOINTS
+    ):
         return  # allow error_blp routes
 
     return redirect(url_for("error_blp.maintenance"))
