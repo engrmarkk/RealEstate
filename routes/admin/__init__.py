b0c121e64eaa91f4251c67a5c8c598cd6a9e6ece
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-02 15:06:05 +0100

    Import agents module in admin blueprint
    Added import for the agents module in the admin blueprint to ensure agent-related routes or logic are registered.

--- routes/admin/__init__.py
+++ routes/admin/__init__.py
@@ -1,6 +1,12 @@
 from flask import Blueprint
 
-admin_blp = Blueprint("admin_blp", __name__, template_folder="../../templates", static_folder="../../static")
+admin_blp = Blueprint(
+    "admin_blp",
+    __name__,
+    template_folder="../../templates",
+    static_folder="../../static",
+)
 
 from .users import *
 from .properties import *
+from .agents import *
