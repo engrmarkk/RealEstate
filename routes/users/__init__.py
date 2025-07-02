9654ad467da0a8bf4e1777b798cedb77da90ae80
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-02 15:07:01 +0100

    Update __init__.py

--- routes/users/__init__.py
+++ routes/users/__init__.py
@@ -1,6 +1,11 @@
 from flask import Blueprint
 
-users_blp = Blueprint("users_blp", __name__, template_folder="../../templates", static_folder="../../static")
+users_blp = Blueprint(
+    "users_blp",
+    __name__,
+    template_folder="../../templates",
+    static_folder="../../static",
+)
 
 from .authorized_route import *
 from .non_authorized_route import *
