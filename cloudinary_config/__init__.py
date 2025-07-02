98636173f01179f91f0ca3549b76aa93c08c8c8d
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-02 15:03:53 +0100

    Update __init__.py

--- cloudinary_config/__init__.py
+++ cloudinary_config/__init__.py
@@ -4,6 +4,8 @@ from constants import CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_
 
 
 cloudinary.config(
-    cloud_name=CLOUDINARY_CLOUD_NAME, api_key=CLOUDINARY_API_KEY, 
-    api_secret=CLOUDINARY_API_SECRET, secure=True
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET,
+    secure=True,
 )
