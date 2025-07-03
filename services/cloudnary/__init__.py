6f64924b795521e2143bdebddae8b6bc4ef92d33
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-03 22:46:26 +0100

    Update __init__.py

--- services/cloudnary/__init__.py
+++ services/cloudnary/__init__.py
@@ -1,11 +1,15 @@
 import cloudinary
 import cloudinary.uploader
 import cloudinary_config
+from constants import CLOUDINARY_UPLOAD_FOLDER
 
 
-def upload_image(f):
-    result = cloudinary.uploader.upload(
-        f, transformation=[{"width": 176, "height": 176, "crop": "fill"}]
-    )
+def upload_image(f, title=None):
+    upload_options = {
+        "folder": CLOUDINARY_UPLOAD_FOLDER,
+    }
+    if title:
+        upload_options["folder"] = f"{CLOUDINARY_UPLOAD_FOLDER}/{title}"
+    result = cloudinary.uploader.upload(f, **upload_options)
     image_url = result["secure_url"]
     return image_url
