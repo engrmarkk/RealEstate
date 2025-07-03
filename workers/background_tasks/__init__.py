3e59378c08e4b74cd8bed1187aa58e97f6f0b077
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-03 22:47:42 +0100

    Enhance image upload task with title and cleanup
    Added a 'title' parameter to the save_property_images task and passed it to the upload_image function. After uploading, the local image files are now deleted to free up disk space, with error handling and logging for file deletion.

--- workers/background_tasks/__init__.py
+++ workers/background_tasks/__init__.py
@@ -1,3 +1,4 @@
+import os
 from models import PropertyImages
 from extensions import db
 from services.cloudnary import upload_image
@@ -6,12 +7,18 @@ from workers.utils.cel_workers import celery, shared_task, send_mail
 
 
 @shared_task
-def save_property_images(property_id, images):
+def save_property_images(property_id, images, title):
     logger.info(f"Saving property images: {property_id}, {images}")
     for image in images:
-        image_url = upload_image(image)
+        image_url = upload_image(image, title=title)
         logger.info(f"Image URL: {image_url} for image: {image}")
         property_image = PropertyImages(image_url=image_url, property_id=property_id)
         db.session.add(property_image)
+        # Delete the file after upload
+        try:
+            os.remove(image)
+            logger.info(f"Deleted local file: {image}")
+        except Exception as e:
+            logger.error(f"Error deleting file {image}: {e}")
     db.session.commit()
     return "Images saved successfully"
