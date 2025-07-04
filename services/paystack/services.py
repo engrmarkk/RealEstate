7ceebbc515fedd255a3dd5fbcae307c4b3ddf277
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-04 18:58:38 +0100

    Update services.py

--- services/paystack/services.py
+++ services/paystack/services.py
@@ -1,5 +1,6 @@
 from .base import PaystackBase
 import requests
+from logger import logger
 
 
 class PaystackEndpoints(PaystackBase):
@@ -10,33 +11,52 @@ class PaystackEndpoints(PaystackBase):
             response.raise_for_status()
             return response.json()
         except Exception as e:
-            print(e, "error from list_banks")
+            logger.error(f"{e}: error from list_banks")
             return {}
 
     def resolve_account(self, account_number, bank_code):
         try:
-            print(self.header, "header from resolve_account")
+            logger.info(f"{self.header}: header from resolve_account")
             url = f"{self.url}/bank/resolve?account_number={account_number}&bank_code={bank_code}"
             response = requests.get(url, headers=self.header)
             response.raise_for_status()  # This will raise an HTTPError for bad responses (4xx and 5xx)
-
-            # Debugging statements
-            print(response.content.decode(), "response content from resolve_account")
-            print(response.json(), "response JSON from resolve_account")
 
             return response.json(), response.status_code
 
         except requests.exceptions.HTTPError as http_err:
             # Handles HTTP errors (4xx, 5xx)
-            print(f"HTTP error occurred: {http_err}")
+            logger.info(f"HTTP error occurred: {http_err}")
             return {}, 500
 
         except requests.exceptions.RequestException as req_err:
             # Handles other requests exceptions (e.g., connection errors)
-            print(f"Request exception occurred: {req_err}")
+            logger.info(f"Request exception occurred: {req_err}")
             return {}, 500
 
         except Exception as e:
             # Handles all other exceptions
-            print(f"An error occurred: {e}")
+            logger.info(f"An error occurred: {e}")
+            return {}, 500
+
+    def verify_transaction(self, reference):
+        """
+        Verifies a Paystack transaction by reference.
+        Returns (response_json, status_code)
+        """
+        try:
+            url = f"{self.url}/transaction/verify/{reference}"
+            logger.info(f"url: {url}")
+            response = requests.get(url, headers=self.header)
+            logger.info(f"g=header: {self.header}")
+            response.raise_for_status()
+            logger.info(f"response: {response.json()}")
+            return response.json(), response.status_code
+        except requests.exceptions.HTTPError as http_err:
+            logger.info(f"HTTP error occurred: {http_err}")
+            return {}, 500
+        except requests.exceptions.RequestException as req_err:
+            logger.info(f"Request exception occurred: {req_err}")
+            return {}, 500
+        except Exception as e:
+            logger.info(f"An error occurred: {e}")
             return {}, 500
