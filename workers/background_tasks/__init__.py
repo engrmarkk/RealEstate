940962bf9c5d321264e1c1d079700ed8213f3419
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-04 18:59:02 +0100

    Add Paystack transaction verification background task
    Introduces a new Celery task, verify_paystack_transaction, to verify Paystack transactions, process cart payments, and clear the user's cart. Errors are logged for troubleshooting.

--- workers/background_tasks/__init__.py
+++ workers/background_tasks/__init__.py
@@ -22,3 +22,21 @@ def save_property_images(property_id, images, title):
             logger.error(f"Error deleting file {image}: {e}")
     db.session.commit()
     return "Images saved successfully"
+
+
+@shared_task
+def verify_paystack_transaction(user_id, ref):
+    try:
+        from services.paystack import pay_stack
+        from cruds import process_cart_payment, add_to_cart
+
+        res = pay_stack.verify_transaction(ref)
+        if res[1] == 200:
+            process_cart_payment(user_id, res[0].get("data"))
+            add_to_cart(user_id, "", "clear_all")
+            return True
+        return False
+    except Exception as e:
+        logger.error(f"Error verifying transaction: {e}")
+        logger.exception(e)
+        return False
