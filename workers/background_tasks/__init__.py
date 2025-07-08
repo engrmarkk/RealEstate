5fdf6094c3e0321d455e2c48bc6caa42890fc4cf
ADENIYI OLANREWAJU <100721103+engrmarkk@users.noreply.github.com>
2025-07-08 12:28:32 +0100

    Send payment receipt email after Paystack verification
    After a successful Paystack transaction, the system now sends a payment receipt email to the user with details of their purchased properties. This includes cart information, total amount, and purchase date, improving user communication and transaction transparency.

--- workers/background_tasks/__init__.py
+++ workers/background_tasks/__init__.py
@@ -4,6 +4,7 @@ from extensions import db
 from services.cloudnary import upload_image
 from logger import logger
 from workers.utils.cel_workers import celery, shared_task, send_mail
+from datetime import datetime
 
 
 @shared_task
@@ -28,11 +29,31 @@ def save_property_images(property_id, images, title):
 def verify_paystack_transaction(user_id, ref):
     try:
         from services.paystack import pay_stack
-        from cruds import process_cart_payment, add_to_cart
+        from cruds import process_cart_payment, add_to_cart, get_user_carts
+        # from workers.utils.cel_workers import send_mail
 
         res = pay_stack.verify_transaction(ref)
         if res[1] == 200:
             process_cart_payment(user_id, res[0].get("data"))
+            user_carts = get_user_carts(user_id)
+            carts = [
+                {
+                    "title": cart.property.title,
+                    "property_type": cart.property.property_type.value.title(),
+                    "amount": cart.property.price * cart.count,
+                    "value": f"{cart.count} plot(s)" if cart.property.property_type.value == "land" else f"{cart.count} unit(s)",
+                } for cart in user_carts
+            ]
+            send_mail({
+                "email": user_carts[0].user.email,
+                "subject": "Your payment is successful",
+                "template_name": "receipts.html",
+                "carts": carts,
+                "full_name": f"{user_carts[0].user.user_profile.last_name} {user_carts[0].user.user_profile.first_name}",
+                "total_amount": sum([cart.property.price * cart.count for cart in user_carts]),
+                "date": datetime.now().strftime("%d %b, %Y"),
+                "year": datetime.now().strftime("%Y"),
+            })
             add_to_cart(user_id, "", "clear_all")
             return True
         return False
